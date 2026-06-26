5dadfb1c93ba216a970c3ed8d0087a2765ff1dd9
kodnova <Dev@KodNova.dev>
2026-06-25 21:44:44 -0500

    add: vite + tailwindcss
    fdsa

---
+++ web_app/src/AddGame.tsx
@@ -0,0 +1,9 @@
+export default function AddGame() {
+	return (
+		<div>
+			<p>Add a Game here</p>
+		</div>
+	);
+}
+
+// NOTE: add a game form? or just add from search?
---
+++ web_app/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+	return (
+		<>
+			<p className="bg-amber-400 text-4xl text-fuchsia-600">hello world</p>
+		</>
+	);
+}

affc25d937ce513cebbafe78ccc3ac9c54ae750e
kodnova <Dev@KodNova.dev>
2026-06-27 01:10:09 -0500

    update: gitignore

--- web_app/src/App.tsx
+++ web_app/src/App.tsx
@@ -1,6 +1,9 @@
+import Header from "./Header";
+
 export default function App() {
 	return (
 		<>
+			<Header />
 			<p className="bg-amber-400 text-4xl text-fuchsia-600">hello world</p>
 		</>
 	);
--- web_app/src/main.tsx
+++ web_app/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createRouter({ routeTree });
+
+const queryClient = new QueryClient();
+
+const root = document.getElementById("root");
+if (!root) throw new Error("Root element not found");
+createRoot(root).render(
+	<StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+		</QueryClientProvider>
+	</StrictMode>,
+);
